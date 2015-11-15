1100e4006e7f93d36fa13e7911036d8d982f5f29
Matt Williams <matwilliams@hotmail.com>
2015-11-15 02:31:19 +0000

    Add Webhook->Websockets gateway

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var simplify = require("simplify-commerce");
+var expressWs = require("express-ws");
 
 SIMPLIFY_CLIENT_CONFIG = {
   publicKey: process.env.SIMPLIFY_PUBLIC_KEY,
@@ -14,31 +15,40 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.raw());
+var wss = expressWs(app);
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, rsp) {
+  rsp.render('pages/index');
 });
 
-app.post('/', function(request, response) {
+app.post('/', function(req, rsp) {
   client = simplify.getClient(SIMPLIFY_CLIENT_CONFIG);
   client.payment.create({amount: "1000",
-                         token: request.body.simplifyToken,
+                         token: req.body.simplifyToken,
                          description: "payment description",
                          currency: "USD"},
-    function(errData, data){
-      if(errData){
+    function(errData, data) {
+      if (errData) {
         console.error("Error Message: " + errData.data.error.message);
         // handle the error
-        response.render('pages/index');
+        rsp.render('pages/index');
         return;
       }
       console.log("Payment Status: " + data.paymentStatus);
-      response.render('pages/index');
+      rsp.render('pages/index');
     });
+});
+
+app.post('/simplify/webhook', function(req, rsp) {
+  wss.getWss('/simplify/websocket').clients.forEach(function(client) {
+    client.send(req.body);
+  });
+});
+
+app.ws('/simplify/websocket', function(ws, req) {
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
